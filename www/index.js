1c91df22fbf65f64f20768d97283a8c5a62d515e
FREDRICK-GITHUB <kamaufredm@gmail.com>
2023-03-12 15:06:33 +0300

    create memory in JS and import it to Wasm example added

--- www/index.js
+++ www/index.js
@@ -1,6 +1,11 @@
 async function init() {
 
+    const memory = new WebAssembly.Memory({initial: 1});
+
     const importObject = {
+        js: {
+            mem: memory
+        },
         console: {
             log: () => {
                 console.log("Just logging something");
@@ -13,11 +18,9 @@ async function init() {
     
     const response = await fetch("sum.wasm");
     const buffer = await response.arrayBuffer();
-    const wasm = await WebAssembly.instantiate(buffer, importObject);
+    await WebAssembly.instantiate(buffer, importObject);
 
-    const sum_function = wasm.instance.exports.sum;
-    const wasm_memory = wasm.instance.exports.memory;
-    const uint8_array = new Uint8Array(wasm_memory.buffer, 0, 2);
+    const uint8_array = new Uint8Array(memory.buffer, 0, 2);
 
     const hi_text = new TextDecoder().decode(uint8_array);
     console.log(hi_text);
