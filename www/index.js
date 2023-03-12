ec9ac1fea47f9fc54c62fde798b74461f7c4555f
FREDRICK-GITHUB <kamaufredm@gmail.com>
2023-03-12 14:27:36 +0300

    added logic to import Javascript into Wasm

--- www/index.js
+++ www/index.js
@@ -1,11 +1,22 @@
 async function init() {
+
+    const importObject = {
+        console: {
+            log: () => {
+                console.log("Just logging something");
+            },
+            error: () => {
+                console.log("I am just an error");
+            }
+        }
+    }
     
     const response = await fetch("sum.wasm");
     const buffer = await response.arrayBuffer();
-    const wasm = await WebAssembly.instantiate(buffer);
-    
+    const wasm = await WebAssembly.instantiate(buffer, importObject);
+
     const sum_function = wasm.instance.exports.sum;
-    const result = sum_function(100, 500);
+    const result = sum_function(100,500);
     console.log(result);
 }
 
