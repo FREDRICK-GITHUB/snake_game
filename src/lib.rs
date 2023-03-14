502cdf6f4cf3c3a0eec7b4db6320b0293c158867
FREDRICK-GITHUB <kamaufredm@gmail.com>
2023-03-14 08:28:10 +0300

    imported the compiled wasm code to our JS, printed name declared in Rust code in browser after compilation

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,11 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    println!("Hi there {}", name);
+    alert(name);
 }
 
-//wasm-pack build --target web
+//importing the JS Alert() to be used to display the name above in greet()
+#[wasm_bindgen]
+extern {
+    pub fn alert(name: &str);
+}
--- www/index.js
+++ www/index.js
@@ -1,29 +1,13 @@
-async function init() {
 
-    const memory = new WebAssembly.Memory({initial: 1});
+import init, { greet } from "snake_game";
 
-    const importObject = {
-        js: {
-            mem: memory
-        },
-        console: {
-            log: () => {
-                console.log("Just logging something");
-            },
-            error: () => {
-                console.log("I am just an error");
-            }
-        }
-    }
-    
-    const response = await fetch("sum.wasm");
-    const buffer = await response.arrayBuffer();
-    await WebAssembly.instantiate(buffer, importObject);
+init().then(_ => {
+    greet("Fredrick");
+})
 
-    const uint8_array = new Uint8Array(memory.buffer, 0, 2);
-
-    const hi_text = new TextDecoder().decode(uint8_array);
-    console.log(hi_text);
-}
-
-init();
+// This is an alternate function to the function above (init)
+// async function start() {
+//     const wasm = await init();
+//     greet("Fredrick");
+//     console.log("OK!");
+// }
