06332cc643fe4157e17542343556edeffc4e88cd
FREDRICK-GITHUB <kamaufredm@gmail.com>
2023-03-27 06:44:03 +0300

    centered the canvas in the display area

--- www/index.js
+++ www/index.js
@@ -3,12 +3,6 @@ import init, { World } from "snake_game";
 
 init().then(_ => {
     const world = World.new();
-    console.log(world.width);
+    const canvas = document.getElementById("snake_canvas");
+    const canvas_context = canvas.getContext("2d");
 })
-
-// This is an alternate function to the function above (init)
-// async function start() {
-//     const wasm = await init();
-//     greet("Fredrick");
-//     console.log("OK!");
-// }
