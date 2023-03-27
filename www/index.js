db0cadc3ee585a164f16184075f68b5cf602fd0d
FREDRICK-GITHUB <kamaufredm@gmail.com>
2023-03-27 08:09:22 +0300

    draw snake on canvas implemented

--- www/index.js
+++ www/index.js
@@ -5,31 +5,46 @@ init().then(_ => {
     const CELL_SIZE = 20;
 
     const world = World.new();
-    const world_width = world.width();
+    const worldWidth = world.width();
 
     const canvas = document.getElementById("snake_canvas");
-    const canvas_ctx = canvas.getContext("2d");
+    const canvasCtx = canvas.getContext("2d");
 
-    canvas.height = world_width * CELL_SIZE;
-    canvas.width = world_width * CELL_SIZE;
+    canvas.height = worldWidth * CELL_SIZE;
+    canvas.width = worldWidth * CELL_SIZE;
 
     function drawWorld() {
         // Draw the rows and the columns for the game
-        canvas_ctx.beginPath();
+        canvasCtx.beginPath();
         
-        for (let x = 0; x < world_width + 1; x++){
-            canvas_ctx.moveTo(CELL_SIZE * x, 0);
-            canvas_ctx.lineTo(CELL_SIZE * x, world_width * CELL_SIZE)
+        for (let x = 0; x < worldWidth + 1; x++){
+            canvasCtx.moveTo(CELL_SIZE * x, 0);
+            canvasCtx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE)
         }
 
-        for (let y = 0; y < world_width + 1; y++){
-            canvas_ctx.moveTo(0, CELL_SIZE * y);
-            canvas_ctx.lineTo(world_width * CELL_SIZE, CELL_SIZE * y)
+        for (let y = 0; y < worldWidth + 1; y++){
+            canvasCtx.moveTo(0, CELL_SIZE * y);
+            canvasCtx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y)
         }
 
-        canvas_ctx.stroke();
+        canvasCtx.stroke();
     }
-    console.log(world.snake_head_index());
+    
+    function drawSnake() {
+        const snakeIndex = world.snake_head_index();
+        const col = snakeIndex % worldWidth;
+        const row = Math.floor(snakeIndex / worldWidth);
+
+        canvasCtx.beginPath();
+        canvasCtx.fillRect(
+            col * CELL_SIZE,
+            row * CELL_SIZE,
+            CELL_SIZE,
+            CELL_SIZE
+        );
+        canvasCtx.stroke();
+    }
 
     drawWorld();
+    drawSnake();
 })
